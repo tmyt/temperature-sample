7efd7d6785b277b3fc0f0cd953b67e54967e98cd
yutaka <shigure@refy.net>
2021-05-02 03:45:46 +0900

    support multi rooms

--- server.js
+++ server.js
@@ -1,25 +1,53 @@
+const fs = require('fs').promises;
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-let temp = 0;
-let humm = 0;
-let updated = new Date(0);
+const data = {};
 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
+app.get('/', (req, res) => {
+  res.send('no content');
 });
-app.get('/put', function(req, res) {
-  temp = req.query.t;
-  humm = req.query.h;
-  updated = new Date();
-  io.emit('data', {temp, humm, updated});
+
+// GET /なにか
+// GET /ほげ
+// GET /room1
+app.get('/:key', async function (req, res) {
+  res.set('content-type', 'text/html');
+  //if (key.startsWith('temp.')) {
+  //  const html = await fs.readFile(__dirname + '/temp.html', 'utf-8');
+  //  res.send(html.replace());
+  //}
+  //else if (key.statsWith('lux.')) {
+  //  const html = await fs.readFile(__dirname + '/lux.html');
+  //  res.send(html.replace());
+  //}
+  const html = await fs.readFile(__dirname + '/index.html', 'utf-8');
+   res.send(html.replace("%%key%%", req.params.key));
+});
+
+// /put?temp=18.22&humm=22.70&key=room1
+// /put?temp=18.22&humm=22.70&key=room2
+// /put?temp=18.22&humm=22.70&key=room3
+// /put?lux=1333&key=step1
+// /put?lux=1333&key=lobby2
+// /put?xxxxxxxxx
+app.get('/put', function (req, res) {
+  const { key, ...queries } = req.query;
+  data[key] = {
+    ...queries,
+    updated: new Date(),
+  };
+  io.to(key).emit('data', data[key]);
   res.sendStatus(200);
 });
 
 io.on('connection', (socket) => {
-  socket.emit('data', {temp, humm, updated});
+  socket.on('init',
+    (key) => {
+      socket.to(key).emit('data', data[key]);
+    });
 });
 
 http.listen(process.env.PORT);

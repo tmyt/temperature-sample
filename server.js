8766739c10f1db8b07b7fc4d5a2cbf1311648da3
yutaka <shigure@refy.net>
2021-05-01 15:16:18 +0900

    show last updated date

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const io = require('socket.io')(http);
 
 let temp = 0;
 let humm = 0;
+let updated = new Date(0);
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
@@ -12,12 +13,13 @@ app.get('/', function(req, res) {
 app.get('/put', function(req, res) {
   temp = req.query.t;
   humm = req.query.h;
-  io.emit('data', {temp, humm});
+  updated = new Date();
+  io.emit('data', {temp, humm, updated});
   res.sendStatus(200);
 });
 
 io.on('connection', (socket) => {
-  socket.emit('data', {temp, humm});
+  socket.emit('data', {temp, humm, updated});
 });
 
 http.listen(process.env.PORT);
